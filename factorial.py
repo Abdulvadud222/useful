79835d76dc7325ab51bbe02955a00972996a5b6f
Abdulvadud21 <Abdulvadud_Akhmadkhanov@student.itpu.uz>
2024-12-10 17:54:49 +0500

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,9 +2,14 @@
 def factorial_c():
     number = int(input("Enter the number to be factorised: "))
     result = 1
-    for i in range(1, number +1):
-        result *= i
-    return f"The factorial of {number} is {result}"
+    if number == 0:
+        print("The factorial of 0 is 1 - it is crazy, isn't it?")
+    elif number < 0:
+        print("It is not possible to factorise negative numbers!")
+    else:
+        for i in range(1, number +1):
+         result *= i
+        return f"The factorial of {number} is {result}"
 print(factorial_c())
 
 
